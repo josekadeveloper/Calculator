ad62ae8eb15a32d1879c2018c00fbb641a7bd11e
josekadeveloper <josekadeveloper@gmail.com>
2023-05-04 00:56:46 +0200

    fix(Calculator): change functionality and distribution of buttons

--- calculator/src/components/Calculator/Calculator.tsx
+++ calculator/src/components/Calculator/Calculator.tsx
@@ -2,96 +2,36 @@ import React, { useState } from "react";
 import { ADD_OPERATOR, SUBTRACT_OPERATOR, MULTIPLY_OPERATOR, DIVIDE_OPERATOR } from "../../utils/Operator";
 
 const Calculator = () => {
-  const [input, setInput] = useState<string>("");
-  const [previousNumber, setPreviousNumber] = useState<number | null>(null);
-  const [currentNumber, setCurrentNumber] = useState<number | null>(null);
-  const [operator, setOperator] = useState<String | null>(null);
+  const [input, setInput] = useState('');
 
-  const handleNumberClick = (number: number) => {
-    setInput(input + number);
-    setCurrentNumber(parseFloat(input + number));
+  const addInput = (val: any) => {
+    setInput(input + val);
   };
 
-  const handleOperatorClick = (operator: String) => {
-    if (currentNumber !== null) {
-      if (previousNumber === null) {
-        setPreviousNumber(currentNumber);
-      } else {
-        switch (operator) {
-          case ADD_OPERATOR:
-            setPreviousNumber(previousNumber + currentNumber);
-            break;
-          case SUBTRACT_OPERATOR:
-            setPreviousNumber(previousNumber - currentNumber);
-            break;
-          case MULTIPLY_OPERATOR:
-            setPreviousNumber(previousNumber * currentNumber);
-            break;
-          case DIVIDE_OPERATOR:
-            setPreviousNumber(previousNumber / currentNumber);
-            break;
-        }
-      }
-    }
-
-    setInput("");
-    setOperator(operator);
-  };
-
-  const handleClearClick = () => {
-    setInput("");
-    setPreviousNumber(null);
-    setCurrentNumber(null);
-    setOperator(null);
-  };
-
-  const handleEqualClick = () => {
-    let result = 0;
-    if (currentNumber !== null && previousNumber !== null && operator !== null) {
-      switch (operator) {
-        case ADD_OPERATOR:
-          result = previousNumber + currentNumber;
-          setInput(`(${previousNumber} ${operator} ${currentNumber}) = ${result}`);
-          break;
-        case SUBTRACT_OPERATOR:
-          result = previousNumber - currentNumber;
-          setInput(`(${previousNumber} ${operator} ${currentNumber}) = ${result}`);
-          break;
-        case MULTIPLY_OPERATOR:
-          result = previousNumber * currentNumber;
-          setInput(`(${previousNumber} ${operator} ${currentNumber}) = ${result}`);
-          break;
-        case DIVIDE_OPERATOR:
-          result = previousNumber / currentNumber;
-          setInput(`(${previousNumber} ${operator} ${currentNumber}) = ${result}`);
-          break;
-      }
-
-      setCurrentNumber(result);
-      setPreviousNumber(null);
-      setOperator(null);
-    }
+  const expCalculate = () => {
+    input ? setInput(eval(input)) : alert("Please enter values to perform calculations.");
   };
 
   return (
     <div className="calculator-container">
       <div className="result">{input}</div>
-      <button className="button" onClick={() => handleNumberClick(1)}>1</button>
-      <button className="button" onClick={() => handleNumberClick(2)}>2</button>
-      <button className="button" onClick={() => handleNumberClick(3)}>3</button>
-      <button className="button" onClick={() => handleNumberClick(4)}>4</button>
-      <button className="button" onClick={() => handleNumberClick(5)}>5</button>
-      <button className="button" onClick={() => handleNumberClick(6)}>6</button>
-      <button className="button" onClick={() => handleNumberClick(7)}>7</button>
-      <button className="button" onClick={() => handleNumberClick(8)}>8</button>
-      <button className="button" onClick={() => handleNumberClick(9)}>9</button>
-      <button className="button" onClick={() => handleNumberClick(0)}>0</button>
-      <button className="button" onClick={() => handleOperatorClick("+")}>+</button>
-      <button className="button" onClick={() => handleOperatorClick("-")}>-</button>
-      <button className="button" onClick={() => handleOperatorClick("*")}>*</button>
-      <button className="button" onClick={() => handleOperatorClick("/")}>/</button>
-      <button className="button" onClick={() => handleClearClick()}>C</button>
-      <button className="button" onClick={() => handleEqualClick()}>=</button>
+      <button className="button" onClick={() => addInput(1)}>1</button>
+      <button className="button" onClick={() => addInput(2)}>2</button>
+      <button className="button" onClick={() => addInput(3)}>3</button>
+      <button className="button" onClick={() => addInput("+")}>+</button>
+      <button className="button" onClick={() => addInput(4)}>4</button>
+      <button className="button" onClick={() => addInput(5)}>5</button>
+      <button className="button" onClick={() => addInput(6)}>6</button>
+      <button className="button" onClick={() => addInput("-")}>-</button>
+      <button className="button" onClick={() => addInput(7)}>7</button>
+      <button className="button" onClick={() => addInput(8)}>8</button>
+      <button className="button" onClick={() => addInput(9)}>9</button>
+      <button className="button" onClick={() => addInput("*")}>*</button>
+      <button className="button" onClick={() => addInput(0)}>0</button>
+      <button className="button" onClick={() => addInput(".")}>.</button>
+      <button className="button" onClick={() => addInput("/")}>/</button>
+      <button className="button" onClick={() => setInput("")}>C</button>
+      <button className="button" onClick={() => expCalculate()}>=</button>
     </div>
   );
 };
