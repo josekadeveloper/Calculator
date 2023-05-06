a1001ac56673306e084cf261462bd05f5a91c2e1
josekadeveloper <josekadeveloper@gmail.com>
2023-05-06 18:06:12 +0200

    feat(Calculator): initial modal window

--- calculator/src/components/Calculator/Calculator.tsx
+++ calculator/src/components/Calculator/Calculator.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-eval */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactModal from "react-modal";
 import { FaTimes } from 'react-icons/fa';
 import './Calculator.scss';
@@ -8,7 +8,8 @@ import Button from "../Button/Button";
 const Calculator = () => {
   const [input, setInput] = useState("");
   const [active, setActive] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [showModal1, setShowModal1] = useState(false);
+  const [showModal2, setShowModal2] = useState(true);
   const lastCharacter = "|";
 
   const addInput = (val: any) => {
@@ -16,7 +17,7 @@ const Calculator = () => {
   };
 
   const expCalculate = () => {
-    input ? setInput(eval(input)) : openModal();
+    input ? setInput(eval(input)) : openModal1();
   };
 
   const turnOn = () => {
@@ -45,15 +46,24 @@ const Calculator = () => {
     setInput(input.slice(0, -1));
   }
 
-  const openModal = () => {
-    setShowModal(true);
-    document.body.classList.add('modal-open');
+  const openModal1 = () => {
+    setShowModal1(true);
   };
 
-  const closeModal = () => {
-    setShowModal(false);
-    document.body.classList.remove('modal-open');
+  const closeModal1 = () => {
+    setShowModal1(false);
   };
+
+  const openModal2 = () => {
+    setShowModal2(true);
+  };
+
+  const closeModal2 = () => {
+    setShowModal2(false);
+
+  };
+
+  useEffect(() => { showModal2 ? openModal2() : closeModal2(); });
 
   const buttons = [
     "ON", "OFF", "DEL", "C",
@@ -67,33 +77,40 @@ const Calculator = () => {
   return (
     <div className="calculator-container">
       {<div className="result"> {input}<span>{active ? lastCharacter : ""}</span></div>}
-      <ReactModal className="modal" overlayClassName="modal-overlay" isOpen={showModal} onRequestClose={() => setShowModal(false)}>
+      <ReactModal className="modal" overlayClassName="modal-overlay" isOpen={showModal1} onRequestClose={() => setShowModal1(false)}>
         <div className="modal-content">
           <h2>TIP</h2>
           <p>To display the result of any operation you need to select numbers and operator</p>
-          <button className="close-button" onClick={closeModal}><FaTimes /></button>
+          <button className="close-button" onClick={closeModal1}><FaTimes /></button>
         </div>
-      </ReactModal >
+      </ReactModal>
+      <ReactModal className="modal" overlayClassName="modal-overlay" isOpen={showModal2} onRequestClose={() => setShowModal2(false)}>
+        <div className="modal-content">
+          <h2>TIP</h2>
+          <p>In order to make use of the different functionalities of the calculator, you must turn it on by clicking on the ON button.</p>
+          <button className="close-button" onClick={closeModal2}><FaTimes /></button>
+        </div>
+      </ReactModal>
       <div className="buttons">
         {buttons.map((button, index) => {
           let buttonElement;
 
           if (button === "ON") {
-            buttonElement = <Button customKey={index} value={button} onClick={() => turnOn()}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => turnOn()} />;
           } else if (button === "OFF") {
-            buttonElement = <Button customKey={index} value={button} onClick={() => turnOff()} disabled={!active}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => turnOff()} disabled={!active} />;
           } else if (button === "DEL") {
-            buttonElement = <Button customKey={index} value={button} onClick={() => handleDelete()} disabled={!active}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => handleDelete()} disabled={!active} />;
           } else if (button === "√") {
-            buttonElement = <Button customKey={index} value={button} onClick={() => handleSqrt()} disabled={!active}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => handleSqrt()} disabled={!active} />;
           } else if (button === "C") {
-            buttonElement = <Button customKey={index} value={button} onClick={() => setInput("")} disabled={!active}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => setInput("")} disabled={!active} />;
           } else if (button === "=") {
-            buttonElement = <Button customKey={index} value={button} onClick={() => expCalculate()} disabled={!active}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => expCalculate()} disabled={!active} />;
           } else if (button === "x²") {
-            buttonElement = <Button customKey={index} value={button} onClick={() => handlePow()} disabled={!active}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => handlePow()} disabled={!active} />;
           } else {
-            buttonElement = <Button customKey={index} value={button} onClick={() => addInput(button)} disabled={!active}/>;
+            buttonElement = <Button customKey={index} value={button} onClick={() => addInput(button)} disabled={!active} />;
           }
 
           return buttonElement;
