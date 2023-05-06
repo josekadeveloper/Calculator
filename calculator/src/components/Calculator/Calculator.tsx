6817de2be0b17088815374a5ce9b96d77f7539e4
josekadeveloper <josekadeveloper@gmail.com>
2023-05-06 10:48:03 +0200

    feat(Calculator): result overflow hidden | add modal window

--- calculator/src/components/Calculator/Calculator.tsx
+++ calculator/src/components/Calculator/Calculator.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import ReactModal from "react-modal";
+import { FaTimes } from 'react-icons/fa';
 
 const Calculator = () => {
   const [input, setInput] = useState("");
   const [active, setActive] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const lastCharacter = "|";
 
   const addInput = (val: any) => {
@@ -10,7 +13,7 @@ const Calculator = () => {
   };
 
   const expCalculate = () => {
-    input ? setInput(eval(input)) : alert("Please enter values to perform calculations.");
+    input ? setInput(eval(input)) : openModal();
   };
 
   const turnOn = () => {
@@ -39,6 +42,16 @@ const Calculator = () => {
     setInput(input.slice(0, -1));
   }
 
+  const openModal = () => {
+    setShowModal(true);
+    document.body.classList.add('modal-open');
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    document.body.classList.remove('modal-open');
+  };
+
   const buttons = [
     "ON", "OFF", "DEL", "C",
     "(", ")", "√", "x²",
@@ -51,6 +64,13 @@ const Calculator = () => {
   return (
     <div className="calculator-container">
       {<div className="result"> {input}<span>{active ? lastCharacter : ""}</span></div>}
+      <ReactModal className="modal" overlayClassName="modal-overlay" isOpen={showModal} onRequestClose={() => setShowModal(false)}>
+        <div className="modal-content">
+          <h2>TIP</h2>
+          <p>To display the result of any operation you need to select numbers and operator</p>
+          <button className="close-button" onClick={closeModal}><FaTimes /></button>
+        </div>
+      </ReactModal >
       <div className="buttons">
         {buttons.map((button, index) => {
           let buttonElement;
--- calculator/src/index.tsx
+++ calculator/src/index.tsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Calculator from './components/Calculator/Calculator';
+import ReactModal  from 'react-modal';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+ReactModal.setAppElement('#root');
 root.render(<Calculator />);
